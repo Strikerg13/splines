f30bfd4ec47c13e843f7afdf3c27574276f24d7d
Matt Gillis <gillism@purdue.edu>
2018-12-17 08:20:35 -0500

    We've got a basic Bezier Curve! so far the translation from 2D to 3D may be as simple as using a Vector 3 instead of a Vector 2. Hopefully that's the case, but we'll see as we move further along. The undo Move Point is not working.

---
+++ splines/Assets/Scripts/PathEditor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+
+    PathCreator creator;
+    Path path;
+
+    void OnSceneGUI()
+    {
+        Draw();
+    }
+
+    void Draw()
+    {
+        for (int i=0; i< path.NumSegments; i++)
+        {
+            Vector3[] points = path.GetPointsInSegment(i);
+
+            // Draws the lines for the handles, connecting the control points.
+            Handles.color = Color.black;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+
+            // draws the green curve
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+
+        }
+
+        // draws the red handles for the control points
+        Handles.color = Color.red;
+        for (int i = 0; i <path.NumPoints; i++)
+        {
+            Vector3 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector3.zero, Handles.CylinderHandleCap);
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");  // supposed to allow you to undo moving of the points (not working)
+                path.MovePoint(i, newPos);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+}
